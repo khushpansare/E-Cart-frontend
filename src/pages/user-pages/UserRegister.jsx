57489a78b1bd36c12f19a6a32d0e587d8053ea07
Khushpansare <hkhushalpansare@gmail.com>
2026-01-08 11:33:45 +0530

    Day-3

---
+++ src/pages/user-pages/UserRegister.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ComponentWrapper from "../../wrapper/ComponentWrapper";
+
+function UserRegister() {
+  return <ComponentWrapper>userRegister</ComponentWrapper>;
+}
+
+export default UserRegister;
